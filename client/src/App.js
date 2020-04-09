b0ae1ce2cc7b5af2c91e7fa5cf22900ad8e19e39
Ethan Lacy <ethanr0m818@gmail.com>
2020-04-08 23:38:12 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* <Switch>
+          <Route exact path={["/", "/books"]} />
+           <Route path="/saved" />
+        </Switch> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
