4bd1f0cb4aaf4e8605f35ae0cc1f7b9f9e6b5b7d
Ethan Lacy <ethanr0m818@gmail.com>
2020-04-10 22:31:50 -0400

    updated packagejson

--- client/public/socket.js
+++ client/public/socket.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3001" || "https://intense-anchorage-26607.herokuapp.com/");
+const socket = io.connect("http://localhost:3001");
 
 const handleClick = e => {
     const element = event.target;
